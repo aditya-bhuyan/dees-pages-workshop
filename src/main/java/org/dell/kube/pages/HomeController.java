eab2f5b2613198f882988c2314bff7d9988b2919
aditya-bhuyan <aditya.sunjava@gmail.com>
2020-07-02 18:47:11 +0530

    Hello Solution to pass TDD

---
+++ src/main/java/org/dell/kube/pages/HomeController.java
@@ -0,0 +1,19 @@
+package org.dell.kube.pages;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping
+    public String getPage(){
+        return "Hello from page : YellowPages";
+    }
+
+
+}
---
+++ src/main/java/org/dell/kube/pages/PageApplication.java
@@ -0,0 +1,13 @@
+package org.dell.kube.pages;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PageApplication.class, args);
+    }
+}
