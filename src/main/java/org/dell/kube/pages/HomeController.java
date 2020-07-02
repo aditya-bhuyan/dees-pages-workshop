09dadc990e68d56d59fd7ab04972affb1eba5dce
aditya-bhuyan <aditya.sunjava@gmail.com>
2020-07-02 20:26:56 +0530

    Configuration Solution

--- src/main/java/org/dell/kube/pages/HomeController.java
+++ src/main/java/org/dell/kube/pages/HomeController.java
@@ -9,10 +9,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/")
 public class HomeController {
 
+    private String pageContent;
 
+    public HomeController(@Value("${page.content}") String pageContent){
+        this.pageContent=pageContent;
+
+    }
     @GetMapping
     public String getPage(){
-        return "Hello from page : YellowPages";
+        return "Hello from page : "+pageContent+" ";
     }
 
 
