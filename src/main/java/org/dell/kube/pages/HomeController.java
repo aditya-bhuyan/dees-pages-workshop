a583466a9d8e46adb28b862dd272f0c0d6f5b58c
aditya-bhuyan <aditya.sunjava@gmail.com>
2020-07-03 15:04:49 +0530

    Loggging Probing and Monitoring Solution

--- src/main/java/org/dell/kube/pages/HomeController.java
+++ src/main/java/org/dell/kube/pages/HomeController.java
@@ -1,5 +1,7 @@
 package org.dell.kube.pages;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/")
 public class HomeController {
-
+    Logger logger = LoggerFactory.getLogger(HomeController.class);
     private String pageContent;
 
     public HomeController(@Value("${page.content}") String pageContent){
@@ -17,6 +19,11 @@ public class HomeController {
     }
     @GetMapping
     public String getPage(){
+        logger.debug("Welcome Page Accessed");
+        logger.info("Welcome Page Accessed");
+        logger.trace("Welcome Page Accessed");
+        logger.warn("Welcome Page Accessed");
+        logger.error("Welcome Page Accessed");
         return "Hello from page : "+pageContent+" ";
     }
 
